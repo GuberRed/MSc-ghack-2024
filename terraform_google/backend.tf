529f546a34c84a2784cd77e22da22c7bcbb9f052
GuberRed <gubermlyn@gmail.com>
2024-02-08 14:23:55 +0100

    project destination change + backend block delete

--- terraform_google/backend.tf
+++ terraform_google/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "gcs" {
-    bucket  = "ghack-infra-tfstate"
-    prefix  = "terraform/state"
-    impersonate_service_account = "tfc-ghack-sa@abel-ghack-infra.iam.gserviceaccount.com"
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket  = "ghack-infra-tfstate"
+#     prefix  = "terraform/state"
+#     impersonate_service_account = "tfc-ghack-sa@abel-ghack-infra.iam.gserviceaccount.com"
+#   }
+# }
