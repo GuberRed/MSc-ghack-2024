7df761d335b6209236b9568e8905695b697c9094
GuberRed <gubermlyn@gmail.com>
2024-02-12 15:04:14 +0100

    subnet fix

--- terraform_google/modules/gke/variables.tf
+++ terraform_google/modules/gke/variables.tf
@@ -2,3 +2,4 @@ variable "ops_project" {type = string}
 variable "prefix" {type = string}
 variable "ops_region" {type = string}
 variable "ops_network" {type = string}
+variable "ops_subnetwork" {type = string}
