0a5a624959bd36fe40638492bf5fbfccb643d34d
GuberRed <gubermlyn@gmail.com>
2024-02-14 11:02:03 +0100

    node count - from 2 to 1 per zone

--- terraform_google/modules/gke/main.tf
+++ terraform_google/modules/gke/main.tf
@@ -18,7 +18,7 @@ resource "google_container_cluster" "ghack_cluster" {
 resource "google_container_node_pool" "ghack_cluster_node_pool" {
   name       = "${var.prefix}-np"
   cluster    = google_container_cluster.ghack_cluster.id
-  node_count = 2
+  node_count = 1
 
   node_config {
     preemptible  = true
