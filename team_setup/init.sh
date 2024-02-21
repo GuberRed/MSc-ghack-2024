e3f4d9784ed89d8adb1ce4b58afb4bcdedd56d34
GuberRed <gubermlyn@gmail.com>
2024-02-21 18:52:34 +0100

    fix iam binding

--- team_setup/init.sh
+++ team_setup/init.sh
@@ -1,6 +1,6 @@
 # Replace these variables with your actual values
 ###########################
-export SERVICE_ACCOUNT_EMAIL="p862555250693-piuc8t@gcp-sa-cloud-sql.iam.gserviceaccount.com"
+export SERVICE_ACCOUNT_EMAIL="tf-oliwer@gcp-coe-msp-sandbox.iam.gserviceaccount.com"
 export PROJECT_ID="abel-ghack-infra"
 ############################
 
