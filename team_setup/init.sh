498d600dbcf7e06787fa3b094bec249a798639cd
GuberRed <gubermlyn@gmail.com>
2024-02-23 17:05:47 +0100

    pasword gen fix

--- team_setup/init.sh
+++ team_setup/init.sh
@@ -1,6 +1,6 @@
 # Replace these variables with your actual values
 ###########################
-export SERVICE_ACCOUNT_EMAIL="snapshoter-bq@gcp-coe-msp-sandbox.iam.gserviceaccount.com"
+export SERVICE_ACCOUNT_EMAIL="insight-tool-testing@gcp-coe-msp-sandbox.iam.gserviceaccount.com"
 export PROJECT_ID="abel-ghack-infra"
 ############################
 
