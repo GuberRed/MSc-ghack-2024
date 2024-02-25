6d595ac975ebf00fa78b51402271a3aa19352111
GuberRed <gubermlyn@gmail.com>
2024-02-25 22:03:40 +0100

    buid db

--- team_setup/publishapp.sh
+++ team_setup/publishapp.sh
@@ -3,4 +3,8 @@ docker build -t gubgub:0.2 .
 docker tag gubgub:0.2 europe-west1-docker.pkg.dev/abel-ghack-infra/ghack-docker-repo/gubgub:latest
 docker push europe-west1-docker.pkg.dev/abel-ghack-infra/ghack-docker-repo/gubgub:latest
 
+docker buildx build --platform linux/amd64 -t gubgub:0.1 .
+docker tag gubgub:0.1 europe-west1-docker.pkg.dev/abel-ghack-infra/ghack-docker-repo/gubgub:0.1
+docker push europe-west1-docker.pkg.dev/abel-ghack-infra/ghack-docker-repo/gubgub:0.1
+
 docker buildx build --push --platform linux/amd64 --tag europe-west1-docker.pkg.dev/abel-ghack-infra/ghack-docker-repo/gubgub:latest .
