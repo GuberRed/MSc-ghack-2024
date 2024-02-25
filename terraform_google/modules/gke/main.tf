793c5f07d230f912d4890652ce144818cd7975d0
GuberRed <gubermlyn@gmail.com>
2024-02-25 19:11:13 +0100

    cluster sa down

--- terraform_google/modules/gke/main.tf
+++ terraform_google/modules/gke/main.tf
@@ -1,8 +1,8 @@
 
-resource "google_service_account" "ghack_cluster_sa" {
-  account_id   = "${var.prefix}-cluster-sa"
-  display_name = "${var.prefix}-cluster-sa"
-}
+# resource "google_service_account" "ghack_cluster_sa" {
+#   account_id   = "${var.prefix}-cluster-sa"
+#   display_name = "${var.prefix}-cluster-sa"
+# }
 
 resource "google_container_cluster" "ghack_cluster" {
   name       = "${var.prefix}-cluster"
@@ -25,17 +25,17 @@ resource "google_container_node_pool" "ghack_cluster_node_pool" {
     preemptible  = true
     machine_type = "e2-standard-2"
 
-    service_account = google_service_account.ghack_cluster_sa.email
+    #service_account = google_service_account.ghack_cluster_sa.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 }
-resource "google_project_iam_binding" "ghack_sa_artifactregistry_viewer" {
-  project = var.ops_project
-  role    = "roles/artifactregistry.viewer"
+# resource "google_project_iam_binding" "ghack_sa_artifactregistry_viewer" {
+#   project = var.ops_project
+#   role    = "roles/artifactregistry.viewer"
 
-  members = [
-    "serviceAccount:${google_service_account.ghack_cluster_sa.email}"
-  ]
-}
+#   members = [
+#     "serviceAccount:${google_service_account.ghack_cluster_sa.email}"
+#   ]
+# }
