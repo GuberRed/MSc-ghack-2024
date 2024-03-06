f01db316845fcf43f4ec96e7e59111fc338f63d4
GuberRed <gubermlyn@gmail.com>
2024-03-06 20:15:53 +0100

    fully working after frontend change

--- scripts/team_setup/solution.sh
+++ scripts/team_setup/solution.sh
@@ -1,9 +1,10 @@
-team_namespace="oliwer-carrefour-test-gcp-coe-msp-sandbox"
+team_namespace="test08-gcp-coe-msp-sandbox"
 kubectl get pods -n $team_namespace
 kubectl describe deployment gubgub-frontend -n $team_namespace
 kubectl get deployment gubgub-frontend -n $team_namespace -o yaml > user_deployment.yaml
 kubectl delete deployment gubgub-frontend -n $team_namespace
 #change password in user_depoloyment
+#OR kubectl exec -it gubgub-db /bin/bash -n $team_namespace // echo $MYSQL_ROOT_PASSWORD #this is cheat way on purpose
 kubectl apply -f user_deployment.yaml
 kubectl describe svc app-service -n $team_namespace | grep "LoadBalancer Ingress"
 kubectl get pods -n $team_namespace
