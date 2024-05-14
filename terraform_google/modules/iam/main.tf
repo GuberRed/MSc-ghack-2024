ce505f1a89da8f80b81e53eb792f247b85610abd
GuberRed <gubermlyn@gmail.com>
2024-05-14 10:23:49 +0200

    germanyiamcustomrole

--- terraform_google/modules/iam/main.tf
+++ terraform_google/modules/iam/main.tf
@@ -1,6 +1,6 @@
 resource "google_project_iam_custom_role" "gke-team-role" {
   project     = var.ops_project
-  role_id     = "${var.prefix}teamrolecustom"
+  role_id     = "${var.prefix}teamrcusgermany"
   title       = "GKE ghack team role"
   permissions = ["container.clusters.get"]
 }
