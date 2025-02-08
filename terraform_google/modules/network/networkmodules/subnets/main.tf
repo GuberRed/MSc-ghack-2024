d04cf4a711d7381342f084dd1c9e134f3e06ec34
GuberRed <gubermlyn@gmail.com>
2025-02-08 20:07:31 +0100

    network fix

--- terraform_google/modules/network/networkmodules/subnets/main.tf
+++ terraform_google/modules/network/networkmodules/subnets/main.tf
@@ -7,17 +7,6 @@ resource "google_compute_subnetwork" "subnetwork" {
   project                  = var.project_id
   description              = var.description
 
-  log_config {
-    aggregation_interval = try(var.subnet_flow_logs_interval, "INTERVAL_5_SEC")
-    flow_sampling        = try(var.subnet_flow_logs_sampling, "0.5")
-    metadata             = try(var.subnet_flow_logs_metadata, "INCLUDE_ALL_METADATA")
-    filter_expr          = try(var.subnet_flow_logs_filter_expr, "true")
-  }
-
-  secondary_ip_range = [
-    for i in range(length(var.secondary_ranges)) : var.secondary_ranges[i]
-  ]
-
   purpose = var.purpose
   role    = var.role
 }
