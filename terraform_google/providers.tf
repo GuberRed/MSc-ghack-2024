e2558f1794bf9d21484a51245001d6e45cc980c0
GuberRed <gubermlyn@gmail.com>
2025-02-08 20:05:17 +0100

    google provider change

--- terraform_google/providers.tf
+++ terraform_google/providers.tf
@@ -7,7 +7,14 @@ terraform {
     }
   }
   required_providers {
-    google = ">= 5.12.0"
+    google = {
+      source = "hashicorp/google"
+      version = ">= 5.12.0"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = ">= 5.12.0"
+    }
   }
 }
 
